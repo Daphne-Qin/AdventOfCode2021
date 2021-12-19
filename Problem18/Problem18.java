17c8bbe099b7e7da247dcba62a4a89b2055dd6c3
Daphne Qin <dqin30@stuy.edu>
2021-12-19 17:12:01 -0500

    Finished P18.2.

--- Problem18/Problem18.java
+++ Problem18/Problem18.java
@@ -6,26 +6,40 @@ public class Problem18 {
   // CODE RUNNER
   //================================================================================
   public static void main(String[] args) throws IOException {
-    //init
+    //init (uses raw because there's no clone method for SnailNumbers)
     Scanner input = new Scanner(new File("input.txt"));
-    ArrayList<SnailNumber> snailNums = new ArrayList<SnailNumber>();
     ArrayList<String> raw = new ArrayList<String>();
     while (input.hasNextLine()) {
       raw.add(input.nextLine());
     }
 
-    for (int i = 0; i < raw.size(); i++) {
-      snailNums.add(new SnailNumber(raw.get(i)));
-    }
 
-    System.out.println("Part 1: " + totalSum(snailNums));
+    // return answers
+    System.out.println("Part 1: " + totalSum(raw));
+    System.out.println("Part 2: " + biggestDoubleSum(raw));
   }
 
-  private static int totalSum(ArrayList<SnailNumber> snailNums) {
-    SnailNumber sum = snailNums.get(0);
-    for (int i = 1; i < snailNums.size(); i++) {
-      sum.add(snailNums.get(i));
+  //================================================================================
+  // PART-SPECIFIC HELPER FUNCTIONS
+  //================================================================================
+  private static int totalSum(ArrayList<String> raw) {
+    SnailNumber sum = new SnailNumber(raw.get(0));
+    for (int i = 1; i < raw.size(); i++) {
+      sum.add(new SnailNumber(raw.get(i)));
     }
     return sum.getMagnitude();
+  }
+
+  private static int biggestDoubleSum(ArrayList<String> raw) {
+    int biggestSum = 0;
+    for (int i = 0; i < raw.size(); i++) {
+      for (int j = 0; j < raw.size(); j++) {
+        SnailNumber s1 = new SnailNumber(raw.get(i));
+        SnailNumber s2 = new SnailNumber(raw.get(j));
+        s1.add(s2);
+        biggestSum = Math.max(biggestSum, s1.getMagnitude());
+      }
+    }
+    return biggestSum;
   }
 }
