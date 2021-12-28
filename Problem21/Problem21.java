8876b7d5b907ee93e0fd604b26c5e9972708593b
Daphne Qin <dqin30@stuy.edu>
2021-12-27 19:20:55 -0500

    Started recursive P22.2.

--- Problem21/Problem21.java
+++ Problem21/Problem21.java
@@ -14,13 +14,51 @@ public class Problem21 {
     }
 
     int part1 = part1(startPos);
+
     // return answers
     System.out.println("Part 1: " + part1);
-    System.out.println("Part 2: " + "N/A");
+    long[] results = winTimes(new int[]{0,0}, startPos, 0);
+    System.out.println("Part 2: " + Math.min(results[0], results[1]));
+    System.out.println(Arrays.toString(results));
 
   }
 
-  private static int part1(int[] startPos) { // naive
+  //================================================================================
+  // RECURSIVE PART 2
+  //================================================================================
+  private static long[] winTimes(int[] scores, int[] pos, int turn) {
+    int[][] rolls = new int[][]{{3, 1}, {4, 3}, {5, 6}, {6, 7}, {7, 6}, {8, 3}, {9, 1}};
+    long[] count = new long[]{0,0};
+
+    for (int[] roll : rolls) {
+      // clean for each iteration
+      int newScores[] = new int[2];
+      int newPos[] = new int[2];
+      for (int i = 0; i < 2; i++) {
+        newScores[i] = scores[i];
+        newPos[i] = pos[i];
+      }
+
+      newPos[turn] = (newPos[turn] + roll[0]) % 10;
+      if (newPos[turn] == 0) newPos[turn] = 10;
+      newScores[turn] += newPos[turn];
+
+      if (newScores[turn] >= 21) {
+        count[turn] += roll[1];
+      } else {
+        turn = (turn == 0) ? 1 : 0;
+        long[] results = winTimes(newScores, newPos, turn);
+        count[0] += results[0];
+        count[1] += results[1];
+      }
+    }
+    return count;
+  }
+
+  //================================================================================
+  // NAIVE PART 1
+  //================================================================================
+  private static int part1(int[] startPos) {
     Player p1 = new Player(startPos[0]);
     Player p2 = new Player(startPos[1]);
 
